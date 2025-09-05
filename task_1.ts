dacec76d3799971cbc049e4f9d877fa8dfd40525
patelTirth991 <tirthpankajpatel@gmail.com>
2025-09-05 17:49:14 +0530

    Add files via upload

---
+++ task_1.ts
@@ -0,0 +1,86 @@
+//Q-1
+var a:number=10,b:number=20;
+console.log(`Addition : ${a+b}`);
+
+//Q-2
+var a:number=20,b:number=10;
+console.log(`Subtraction : ${a-b}`);
+
+//Q-3
+var a:number=20,b:number=10;
+console.log(`Multiplication : ${a*b}`);
+
+//Q-4
+var a:number=20,b:number=10;
+console.log(`Division : ${a/b}`);
+
+//Q-5
+var a:number=20,b:number=3;
+console.log(`reminder : ${a%b}`);
+
+//Q-6
+var a:number=20;
+console.log(`After Increment : ${++a}`);
+
+//Q-7
+var a:number=20;
+console.log(`After Decrement : ${--a}`);
+
+//Q-8
+var length:number=20,width:number=10;
+console.log(`Area of rectangle : ${length*width}`);
+
+//Q-9
+var a:number=20,b:number=10,c:number=30;
+console.log(`Average : ${(a+b+c)/3}`);
+
+//Q-10
+//with variable
+var a:number=20,b:number=10,c:number=undefined;
+c=a
+a=b
+b=c
+console.log(`a : ${a}`);
+console.log(`b : ${b}`);
+
+//without variable
+var a:number=20,b:number=10;
+a=a+b
+b=a-b
+a=a-b
+console.log(`a : ${a}`);
+console.log(`b : ${b}`);
+
+//Q-11
+var C:number=30;
+var F:number=(C*9/5)+32;
+console.log(`Fahrenheit : ${F}`);
+
+//Q-12
+var radius:number=4.5;
+const pie:number=3.14;
+console.log(`Area of Circle : ${pie*radius*radius}`);
+
+//Q-13
+var side:number=20;
+console.log(`Area of Square : ${side*side}`);
+
+//Q-14
+var p:number=50000,r:number=7.5,t:number=4;
+console.log(`Simple Intrest : ${(p*r*t)/100}`);
+
+//Q-15
+var BS:number=50000,hra:number=BS*0.1,DA:number=BS*0.5;
+console.log(`Total Salary : ${BS+hra+DA}`);
+
+//Q-16
+var Price:number=50,Unit:number=100,bill:number=Price*Unit;
+console.log(`Total bill : ${bill}`);
+
+//Q-17
+var length:number=15,width:number=10;
+console.log(`Perimetre of rectangle : ${2*(length+width)}`);
+
+//Q-18
+var radius:number=4.5,pi:number=3.14;
+console.log(`Perimetre of Circle : ${2*pi*radius}`);
